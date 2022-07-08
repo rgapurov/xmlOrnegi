0e69d89fc598c769c5d7a0f2302c885fb03479b0
rgapurov <92965000+rgapurov@users.noreply.github.com>
2022-07-08 10:48:47 +0300

    Add project files.

---
+++ xmlOrnegi/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace xmlOrnegi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblAd.Location = new System.Drawing.Point(168, 125);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(44, 31);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtAd.Location = new System.Drawing.Point(228, 122);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(182, 38);
+            this.txtAd.TabIndex = 1;
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSoyad.Location = new System.Drawing.Point(133, 192);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(79, 31);
+            this.lblSoyad.TabIndex = 0;
+            this.lblSoyad.Text = "Soyad";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtSoyad.Location = new System.Drawing.Point(228, 189);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(182, 38);
+            this.txtSoyad.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCreate.Location = new System.Drawing.Point(197, 290);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(168, 54);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Oluştur";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 464);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ xmlOrnegi/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace xmlOrnegi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            // xml dosyamın oluşacak yolunu proje yolu olarak belirttim
+            string xmlpath = Application.StartupPath + "\\Customer.xml";
+            //Yazıcı nesnemi ve formatımı oluşturdum
+            XmlTextWriter customer = new XmlTextWriter(xmlpath, UTF8Encoding.UTF8);
+            //Format düzenleme satırımı ekledım
+            customer.Formatting = Formatting.Indented;
+            //ve işlemimi başlatıyorum
+            customer.WriteStartDocument();
+            customer.WriteStartElement("Müşteriler");
+            customer.WriteStartElement("Müşteri");
+            //Id Tanımlamak için kullandım
+            customer.WriteStartAttribute("ID", "1");
+            //Sıra geldi Textboxlardan verileri almaya!!
+            customer.WriteElementString("isim", txtAd.Text);
+            customer.WriteElementString("soyisim", txtSoyad.Text);
+            //tag kapama işlemimi yapıyorum
+            customer.WriteEndElement();
+            customer.WriteEndElement();
+            //Kapıyorum
+            customer.Close();
+            MessageBox.Show("İŞLEMİNİZ TAMAMLANMIŞTIR");
+            Application.Exit();
+        }
+    }
+}
